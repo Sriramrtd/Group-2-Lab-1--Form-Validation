301188a766820c2659aec7d9d7a4508b8ce45bdf
SumanthEadula <59619965+SumanthEadula@users.noreply.github.com>
2020-10-11 00:57:52 -0400

    Final validations
    Php validations
    Sumanth

--- fun.php
+++ fun.php
@@ -28,7 +28,7 @@ $state=$_GET["state"];
 $pincode=$_GET["pinCode"];
 
 
-
+$height=$_GET["height"];
 
 if(!preg_match("/[a-zA-Z]{4,30}$/", $firstname))
 {
@@ -64,6 +64,63 @@ if(!preg_match("/[a-zA-Z]{4,30}$/", $state))
 {
     $errors[]="Enter the valid state";        
 }
+
+if(!preg_match("/[0-9]{2,3}$/", $height))
+{
+    $errors[]="Enter the valid height";        
+}
+
+
+
+
+if(count($errors) === 0) {
+    echo "<script>alert('Gym Registration successfull')</script>";
+    } else {
+        foreach($errors as $error) {
+            echo '<script>alert("'.$error.'");</script>';
+        }
+
+
+    }
+
+            
+
+}
+
+if(isset($_POST['upload'])){
+    $file=$_FILES['file'];
+
+    $fileName= $_FILES['file']['name'];
+    $fileTmpName= $_FILES['file']['tmp_name'];
+    $fileSize= $_FILES['file']['size'];
+    $fileError= $_FILES['file']['error'];
+    $fileType= $_FILES['file']['type'];
+
+    $fileExt = explode('.',$fileName);
+    $fileActualExt=strtolower(end($fileExt));
+
+    $allowed= array('jpg','jpeg','png');
+
+    if(in_array($fileActualExt,$allowed)){
+        if($fileError===0){
+            if($fileSize < 500000){
+                $fileNameNew = uniqid('',true).".".$fileActualExt;
+                $fileDestination = 'uploads/'.$fileNameNew;
+                move_uploaded_file($fileTmpName,$fileDestination);
+            
+            }else{
+                echo "Your file is too big!";
+            }
+        }else{
+            echo "There was an error uploading your file!";
+        }
+
+    }else{
+        echo "You cannot upload files of this type!";
+    }
+
+
+
 
 
 }
