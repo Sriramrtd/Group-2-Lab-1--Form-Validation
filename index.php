508e09f1a111f0947126eece57108d4ce973b759
Suryanethra <59619925+Suryanethra@users.noreply.github.com>
2020-10-10 23:37:55 -0400

    Create index.php
    html validation
    Surya

---
+++ index.php
@@ -0,0 +1,85 @@
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.3.0/ekko-lightbox.css" />
+	<link rel="stylesheet" href="style.css">
+
+    <script src="script.js"></script>
+	<title>Login Panel</title>
+</head>
+
+<body>
+	
+	<!-- PAGE HEADER -->
+	<header id="page-header">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 m-auto text-center">
+					<h1>GYM Registration Panel</h1>
+				</div>
+			</div>
+		</div>
+	</header>
+     <?php
+          require_once "fun.php";
+     ?>
+		<div class="container">
+			<form action=""  name="f1" method="POST" onsubmit="return validate()" enctype="multipart/form-data">
+			
+								
+				<h4><p style="text-align:center; background-color: #cc6e00; padding: 10px; color: #ffffff; margin: auto 0px; border-radius: 0.25rem;">General Details</p></h4>
+
+				<div class="form-group">
+					<label for="inputFirstName">First Name: </label>
+					<input type="text" id="inputFirstName"  placeholder="First Name" name="firstName" class="form-control" required="required">	
+				</div>
+				
+				<div class="form-group">
+					<label for="inputLastUnit">Last Name: </label>
+					<input type="text" id="inputLastUnit" placeholder="Last Name" name="lastName" class="form-control" required="required">
+				</div>
+				
+				<div class="form-group">
+					<label for="inputMobNo">Mobile Number: </label>
+					<input type="tel" id="inputMobNo" pattern="(6|7|8|9)\d{9}"  name="phoneNumber" placeholder="Eg. 90090090000" class="form-control" required="required">
+				</div>
+				
+				<div class="form-group">
+					<label for="inputAge">Age: </label>
+					<input type="number" id="inputAge" min="16" max="50" placeholder="Age" name="age" class="form-control" required="required">
+				</div>
+				
+				
+
+				
+
+
+					
+			</form>
+			
+			<br>
+			
+		</div>
+		
+	</div>
+
+	
+
+
+	<script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+
+
+	<script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
