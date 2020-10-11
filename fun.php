fc61bdae39b469bb904df8c50f7b67abc4370d3c
Sriramrtd <sriram.rathod@icloud.com>
2020-10-11 00:54:05 -0400

    Update fun.php
    Php Validations
    
    Sri ram

--- fun.php
+++ fun.php
@@ -16,6 +16,18 @@ $phonenumber=$_GET["phoneNumber"];
 
 $age=$_GET["age"];
 
+$address=$_GET["address"];
+
+
+$city=$_GET["city"];
+
+
+$state=$_GET["state"];
+
+
+$pincode=$_GET["pinCode"];
+
+
 
 
 if(!preg_match("/[a-zA-Z]{4,30}$/", $firstname))
@@ -38,7 +50,20 @@ if(!preg_match("/[0-9]{2}$/", $age))
     $errors[]="Enter the valid age";        
 }
 
+if(!preg_match("/[a-zA-Z0-9\s,.'-]{3,}$/", $address))
+{
+    $errors[]="Enter the valid age";        
+}
 
+if(!preg_match("/[a-zA-Z]{4,30}$/", $city))
+{
+    $errors[]="Enter the valid city";        
+}
+
+if(!preg_match("/[a-zA-Z]{4,30}$/", $state))
+{
+    $errors[]="Enter the valid state";        
+}
 
 
 }
